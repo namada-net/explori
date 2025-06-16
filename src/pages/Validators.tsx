f9337a3ec9f12dbba76fcf927c151111a8bb24f2
neocybereth <sean@arrowburn.co.nz>
2025-06-16 12:09:08 +1200

    fix: cleanup

--- src/pages/Validators.tsx
+++ src/pages/Validators.tsx
@@ -318,16 +318,6 @@ export const Validators = () => {
                                         e.currentTarget.style.display = "none";
                                       }}
                                     />
-                                    <Text
-                                      fontSize="xs"
-                                      color="gray.300"
-                                      position="absolute"
-                                      top="50%"
-                                      left="50%"
-                                      transform="translate(-50%, -50%)"
-                                    >
-                                      {(validator.name || "U")[0].toUpperCase()}
-                                    </Text>
                                   </>
                                 ) : (
                                   <Text fontSize="xs" color="gray.300">
