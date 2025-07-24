7a2b157e0d45b7e40f09cbd0a17730c55725bec5
brentstone <brentstone01@gmail.com>
2025-07-24 01:49:06 -0700

    Details section always on right column

--- src/components/ProposalContentCard.tsx
+++ src/components/ProposalContentCard.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@chakra-ui/react";
+import { Grid, Box, VStack } from "@chakra-ui/react";
 import type { ProposalContent } from "../types";
 import { Data } from "./Data";
 import { linkifyText } from "../utils/linkify";
@@ -27,58 +27,65 @@ export const ProposalContentCard = ({
       borderColor="yellow"
       overflow="auto"
     >
-      {hasContent(proposalContent.title) && (
-        <Data
-          title="Title"
-          content={proposalContent.title}
-        />
-      )}
-      {hasContent(proposalContent.abstract) && (
-        <Data
-          title="Abstract"
-          content={proposalContent.abstract}
-        />
-      )}
-      {hasContent(proposalContent.authors) && (
-        <Data
-          title="Authors"
-          content={proposalContent.authors}
-        />
-      )}
-      {hasContent(proposalContent.created) && (
-        <Data
-          title="Created on"
-          content={proposalContent.created}
-        />
-      )}
-      {hasContent(proposalContent.license) && (
-        <Data
-          title="License"
-          content={proposalContent.license}
-        />
-      )}
-      {hasContent(proposalContent["discussions-to"]) && (
-        <Data
-          title="Discussions to"
-          content={proposalContent["discussions-to"]}
-        />
-      )}
-      {hasContent(proposalContent.details) && (
-        <Data
-          title="Details"
-          content={
-            <span style={{ whiteSpace: 'pre-line' }}>
-              {linkifyText(proposalContent.details!.replace(/\\n/g, '\n'))}
-            </span>
-          }
-        />
-      )}
-      {hasContent(proposalContent.motivation) && (
-        <Data
-          title="Motivation"
-          content={proposalContent.motivation}
-        />
-      )}
+      {/* Left Column - All fields except Details */}
+      <VStack align="stretch" gap={4}>
+        {hasContent(proposalContent.title) && (
+          <Data
+            title="Title"
+            content={proposalContent.title}
+          />
+        )}
+        {hasContent(proposalContent.abstract) && (
+          <Data
+            title="Abstract"
+            content={proposalContent.abstract}
+          />
+        )}
+        {hasContent(proposalContent.authors) && (
+          <Data
+            title="Authors"
+            content={proposalContent.authors}
+          />
+        )}
+        {hasContent(proposalContent.created) && (
+          <Data
+            title="Created on"
+            content={proposalContent.created}
+          />
+        )}
+        {hasContent(proposalContent.license) && (
+          <Data
+            title="License"
+            content={proposalContent.license}
+          />
+        )}
+        {hasContent(proposalContent["discussions-to"]) && (
+          <Data
+            title="Discussions to"
+            content={proposalContent["discussions-to"]}
+          />
+        )}
+        {hasContent(proposalContent.motivation) && (
+          <Data
+            title="Motivation"
+            content={proposalContent.motivation}
+          />
+        )}
+      </VStack>
+
+      {/* Right Column - Details only */}
+      <Box>
+        {hasContent(proposalContent.details) && (
+          <Data
+            title="Details"
+            content={
+              <span style={{ whiteSpace: 'pre-line' }}>
+                {linkifyText(proposalContent.details!.replace(/\\n/g, '\n'))}
+              </span>
+            }
+          />
+        )}
+      </Box>
     </Grid>
   );
 };
