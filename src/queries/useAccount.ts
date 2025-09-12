f970959c02bf57b38a047c68aae5380d4761931c
iskay <ikay@lakeheadu.ca>
2025-09-12 11:51:02 -0400

    update useAccount for new indexer response format

--- src/queries/useAccount.ts
+++ src/queries/useAccount.ts
@@ -1,9 +1,22 @@
-import { useSimpleGet } from "./useSimpleGet";
+import { useQuery } from "@tanstack/react-query";
+import { get } from "../http/query";
 
 export const useAccount = (address: string) => {
-  return useSimpleGet(
-    `account-${address}`,
-    `/account/${address}`,
-    undefined // Don't auto-refetch account data
-  );
+	return useQuery({
+		queryKey: ["account", address],
+		queryFn: async () => {
+			const data = await get(`/account/${address}`);
+			if (Array.isArray(data)) {
+				return data.map((item: any) => ({
+					...item,
+					tokenAddress:
+						typeof item?.tokenAddress === "string"
+							? item.tokenAddress
+							: item?.tokenAddress?.address ?? "",
+				}));
+			}
+			return data;
+		},
+		refetchInterval: undefined,
+	});
 };
